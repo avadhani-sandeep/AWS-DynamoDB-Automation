835bbd350f57ca956b0c25f6817ff633b6bd39eb
Sandeep Avadhani <vrukodharaavdhani@gmail.com>
2025-11-17 15:50:35 +0530

    Create main.tf

---
+++ DynamoDB Automation/terraform-setup/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_dynamodb_table" "todo_tasks" {
+  name           = "todo_tasks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "task_id"
+
+  attribute {
+    name = "task_id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
+
+resource "aws_sns_topic" "todo_notifications" {
+  name = "todo_notifications"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.todo_notifications.arn
+  protocol  = "email"
+  endpoint  = "sandeepavadhani94@gmail.com"   
+}
+
+output "sns_topic_arn" {
+    value = aws_sns_topic.todo_notifications.arn
+    description = "The ARN of the sns Topic"
+}
